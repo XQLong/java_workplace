8f22fd6f56a35736788a6c6cc7401fb7d1b22c57
XQLong <xql820148218@gmail.com>
2018-11-20 15:27:12 +0800

    求交叉和

---
+++ src/cross_add/cross_add.java
@@ -0,0 +1,38 @@
+package cross_add;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+/**
+ * Created by xql on 2018/11/19.
+ */
+public class cross_add {
+    public static void main(String args[]) throws IOException {
+        BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
+        String data =scanner.readLine();
+        String[] num = data.split(" ");
+        int ans = 0;
+        int l = Integer.valueOf(num[0]);
+        int r = Integer.valueOf(num[1]);
+        int v = Integer.valueOf(num[2]);
+        for (int i = l;i <= r;i++){
+            if(v==add(i)){
+                ans += i;
+            }
+        }
+        System.out.println(ans%1000000000);
+    }
+    public static int add(int a){
+        double ans = 0;
+        String num = String.valueOf(a);
+        char[] c = num.toCharArray();
+        for (int i=0;i<c.length;i++){
+            double par = Math.pow(-1,i);
+            int d = c[i]-'0';
+            ans += par*d;
+        }
+        return (new Double(ans).intValue());
+    }
+}

68c3a52a0d1e3ccd76a210ba0e299c4f4a30638c
XQLong <xql820148218@gmail.com>
2019-03-21 15:55:58 +0800

    commit

--- src/JavaBasicExam/ThreadTest.java
+++ src/JavaBasicExam/ThreadTest.java
@@ -3,7 +3,7 @@ package JavaBasicExam;
 /**
  * Created by xql on 2019/3/5.
  */
-public class TreadTest extends Thread{
+public class ThreadTest extends Thread{
     private int i;
 
     @Override
@@ -17,8 +17,8 @@ public class TreadTest extends Thread{
         for(int i=0;i<100;i++){
             System.out.println(currentThread()+" "+i);
             if(i==20){
-                new TreadTest().start();
-                new TreadTest().start();
+                new ThreadTest().start();
+                new ThreadTest().start();
             }
         }
     }

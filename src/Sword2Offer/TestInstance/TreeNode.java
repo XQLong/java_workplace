1d43a716d17960711b9589e3091de0bac9af1fb5
XQLong <xql820148218@gmail.com>
2019-03-24 19:55:32 +0800

    二叉树测试用例

---
+++ src/Sword2Offer/TestInstance/TreeNode.java
@@ -0,0 +1,13 @@
+package Sword2Offer.TestInstance;
+
+/**
+ * Created by xql on 2019/3/24.
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x){
+        this.val = x;
+    }
+}

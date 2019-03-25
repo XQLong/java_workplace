b7a3f9c62bd0606fbe2e5f0a6ad4b5b8ae7529b5
XQLong <xql820148218@gmail.com>
2019-03-25 22:34:08 +0800

    带有上一节点指针的二叉树测试用例

---
+++ src/Sword2Offer/TestInstance/TreeLinkNode.java
@@ -0,0 +1,15 @@
+package Sword2Offer.TestInstance;
+
+/**
+ * Created by xql on 2019/3/25.
+ */
+public class TreeLinkNode {
+    public int val;
+    public TreeLinkNode left = null;
+    public TreeLinkNode right = null;
+    public TreeLinkNode next = null;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+}

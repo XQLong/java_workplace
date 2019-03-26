97d277d1601a131f63e04977e5258f9140cf0fb6
XQLong <xql820148218@gmail.com>
2019-03-26 16:47:44 +0800

    斐波那契数列

---
+++ src/Sword2Offer/fibonacci_10/Fibonacci.java
@@ -0,0 +1,30 @@
+package Sword2Offer.fibonacci_10;
+
+/**
+ * Created by xql on 2019/3/26.
+ */
+public class Fibonacci {
+    public int fibonacci(int n) {
+        if(n==0) return n;
+        int[] arr = new int[n+1];
+        arr[1] = 1;
+        for (int i=2;i<=n;i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return arr[n];
+    }
+    public int fibonacci1(int n) {
+        if (n<=1) return n;
+        int pre1 = 1;
+        int pre2 = 0;
+        int i=1;
+        int res = 0;
+        while(i!=n){
+            res = pre1 + pre2;
+            pre2 = pre1;
+            pre1 = res;
+            i++;
+        }
+        return res;
+    }
+}

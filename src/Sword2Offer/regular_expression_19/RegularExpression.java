85fc558d0fd182ef555fc56e4dc499fd9933ad60
XQLong <xql820148218@gmail.com>
2019-03-27 22:38:50 +0800

    正则表达式

---
+++ src/Sword2Offer/regular_expression_19/RegularExpression.java
@@ -0,0 +1,17 @@
+package Sword2Offer.regular_expression_19;
+
+/**
+ * Created by xql on 2019/3/27.
+ */
+public class RegularExpression {
+    public boolean match(char[] str, char[] pattern)
+    {
+        int ls = str.length; int lp = pattern.length;
+        for(int i=0;i<lp;i++){
+            if(i!=(lp-1)&&pattern[i]!='.'&&pattern[i]!='*'){
+
+            }
+        }
+        return false;
+    }
+}

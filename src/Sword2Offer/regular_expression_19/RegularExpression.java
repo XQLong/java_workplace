f7a6a3ff3030af606b942b394e05ddedbcaca7a6
XQLong <xql820148218@gmail.com>
2019-03-28 22:51:11 +0800

    正则表达式

--- src/Sword2Offer/regular_expression_19/RegularExpression.java
+++ src/Sword2Offer/regular_expression_19/RegularExpression.java
@@ -7,11 +7,44 @@ public class RegularExpression {
     public boolean match(char[] str, char[] pattern)
     {
         int ls = str.length; int lp = pattern.length;
+        char[] newpattern = new char[ls];
+        if(pattern[0]=='*') return false;
         for(int i=0;i<lp;i++){
-            if(i!=(lp-1)&&pattern[i]!='.'&&pattern[i]!='*'){
-
+            if(pattern[i]!='.'&&pattern[i+1]!='*'){
+                if(pattern[i] != str[i]) return false;
+                newpattern[i] = str[i];
+            }else if(pattern[i]=='.'){
+                newpattern[i] = str[i];
+            }else if(pattern[i]!='.'&&pattern[i+1]=='*'){
+                if(pattern[i] != str[i]) newpattern[i] = pattern[i+1];
             }
         }
         return false;
+    }
+    public char[][] buildnewpattern(char[] pattren,int ind,boolean is){
+        int l =pattren.length;
+        char[][] newpattern = new char[2][l];
+        //如果*前字母相等，则去掉*并添加其前面的字母
+        if(is){
+            for (int i=0;i<l;i++){
+                if(i>=(ind-1)&&(i+2)<l){
+                    newpattern[1][i] = pattren[i+2];
+                }
+                if(i>=ind){
+                    newpattern[1][i] = pattren[i-1];
+                }else {
+                    newpattern[1][i] = pattren[i];
+                }
+            }
+        }
+        //如果*前字母不相等，则去掉*及其前面的字母
+        else {
+            for (int i=0;i<l;i++){
+                if(i>=(ind-1)&&(i+2)<l){
+                    newpattern[1][i] = pattren[i+2];
+                }
+                newpattern[1][i] = pattren[i];
+            }
+        }
     }
 }

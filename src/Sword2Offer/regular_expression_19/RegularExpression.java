85ce4d44b99cd65a1aa1ff42284cbb3a2f29a085
XQLong <xql820148218@gmail.com>
2019-03-29 12:11:55 +0800

    朝着

--- src/Sword2Offer/regular_expression_19/RegularExpression.java
+++ src/Sword2Offer/regular_expression_19/RegularExpression.java
@@ -6,45 +6,30 @@ package Sword2Offer.regular_expression_19;
 public class RegularExpression {
     public boolean match(char[] str, char[] pattern)
     {
-        int ls = str.length; int lp = pattern.length;
-        char[] newpattern = new char[ls];
-        if(pattern[0]=='*') return false;
-        for(int i=0;i<lp;i++){
-            if(pattern[i]!='.'&&pattern[i+1]!='*'){
-                if(pattern[i] != str[i]) return false;
-                newpattern[i] = str[i];
-            }else if(pattern[i]=='.'){
-                newpattern[i] = str[i];
-            }else if(pattern[i]!='.'&&pattern[i+1]=='*'){
-                if(pattern[i] != str[i]) newpattern[i] = pattern[i+1];
+        return false;
+    }
+    public boolean matchpattern(char[] str,char[] pattren,int indstr,int indpattern){
+        int ls = str.length;
+        int lp = pattren.length;
+        if(indstr==ls&&indpattern==lp) return true;
+        if (indpattern>=lp) return false;
+        if(pattren[indpattern+1]!='*'){
+            if(str[indstr]==pattren[indpattern]||str[indstr]=='.'){
+                matchpattern(str,pattren,indstr+1,indpattern+1);
+            }else if(str[indstr]!=pattren[indpattern]&&str[indstr]!='.'){
+                return false;
+            }
+        }else if(pattren[indpattern+1]=='*'){
+            if(str[indstr]==pattren[indpattern]||str[indstr]=='.'){
+                return (matchpattern(str, pattren, indstr, indpattern+2)||matchpattern(str, pattren, indstr+1, indpattern+2)||matchpattern(str, pattren, indstr+1, indpattern));
+            }else if(str[indstr]!=pattren[indpattern]&&str[indstr]!='.'){
+                if(str[indstr]==pattren[indpattern+2]){
+                    matchpattern(str, pattren, indstr+1, indpattern+3);
+                }else {
+                    return false;
+                }
             }
         }
         return false;
-    }
-    public char[][] buildnewpattern(char[] pattren,int ind,boolean is){
-        int l =pattren.length;
-        char[][] newpattern = new char[2][l];
-        //如果*前字母相等，则去掉*并添加其前面的字母
-        if(is){
-            for (int i=0;i<l;i++){
-                if(i>=(ind-1)&&(i+2)<l){
-                    newpattern[1][i] = pattren[i+2];
-                }
-                if(i>=ind){
-                    newpattern[1][i] = pattren[i-1];
-                }else {
-                    newpattern[1][i] = pattren[i];
-                }
-            }
-        }
-        //如果*前字母不相等，则去掉*及其前面的字母
-        else {
-            for (int i=0;i<l;i++){
-                if(i>=(ind-1)&&(i+2)<l){
-                    newpattern[1][i] = pattren[i+2];
-                }
-                newpattern[1][i] = pattren[i];
-            }
-        }
     }
 }

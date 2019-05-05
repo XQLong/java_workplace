44b7dacb20c40d96230674ffe24f187cb89c9d37
XQLong <xql820148218@gmail.com>
2019-05-05 09:27:58 +0800

    添加复杂链表用例

---
+++ src/Sword2Offer/TestInstance/RandomListNode.java
@@ -0,0 +1,13 @@
+package Sword2Offer.TestInstance;
+
+/**
+ * Created by xql on 2019/5/5.
+ */
+public class RandomListNode {
+    int lable;
+    RandomListNode next = null;
+    RandomListNode Random = null;
+    RandomListNode(int lable){
+        this.lable = lable;
+    }
+}

b337aa94c3cdeade4a3a4677cc414b1c29b8cd48
XQLong <xql820148218@gmail.com>
2019-06-15 11:51:59 +0800

    新增快速排序算法

--- src/Algorithms/SortMethod/Test.java
+++ src/Algorithms/SortMethod/Test.java
@@ -5,12 +5,13 @@ package Algorithms.SortMethod;
  */
 public class Test {
     public static void main(String[] args) {
-        Comparable[] input = new Comparable[]{5, 4,6, 2, 1,7, 8, 3};
+        Comparable[] input = new Comparable[]{5, 4,6, 2, 1,7, 8, 3,5,9,12,8,0,2,5,7,2,1,4,8,1,78};
         SortMethod test = new SortMethod();
 //        Comparable[] arr1 = test.chooseSort(input);
 //        Comparable[] arr2 = test.insertSort(input);
 //        Comparable[] arr3 = test.shellSort(input);
 //        Comparable[] arr4 = test.mergeSort(input);
-          Comparable[] arr5 = test.bottom_up_mergeSort(input);
+//        Comparable[] arr5 = test.bottom_up_mergeSort(input);
+          Comparable[] arr6 = test.quickSort(input);
     }
 }

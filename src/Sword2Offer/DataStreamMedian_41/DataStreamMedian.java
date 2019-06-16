9ce47c2830274215fd87310011ec7c0f6680a312
XQLong <xql820148218@gmail.com>
2019-06-16 20:36:09 +0800

    数据流中的中位数

---
+++ src/Sword2Offer/DataStreamMedian_41/DataStreamMedian.java
@@ -0,0 +1,14 @@
+package Sword2Offer.DataStreamMedian_41;
+
+/**
+ * Created by xql on 2019/6/16.
+ */
+public class DataStreamMedian {
+    public void Insert(Integer num) {
+
+    }
+
+    public Double GetMedian() {
+        return 0.0;
+    }
+}

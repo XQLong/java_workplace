6fa88ec1e25c8206be41d4e69c7cf4b53dbc08bf
XQLong <xql820148218@gmail.com>
2019-07-11 10:33:23 +0800

    工厂方法模式抽象工厂类

---
+++ src/Design_Pattern/Creational_Pattern/Factory.java
@@ -0,0 +1,16 @@
+package Design_Pattern.Creational_Pattern;
+
+import Design_Pattern.Interface.Product;
+
+/**
+ * @Author: xql
+ * @Date: Created in 20:25 2019/7/10
+ * 工厂方法模式中的抽象工厂类
+ */
+public abstract class Factory {
+    abstract public Product factoryMethod();
+    public void doSomething(){
+        Product product = factoryMethod();
+        //do something with the product
+    }
+}

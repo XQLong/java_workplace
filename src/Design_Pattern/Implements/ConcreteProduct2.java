376ff2aca9ba71f42b59c18ad67dd10b00c2f991
XQLong <xql820148218@gmail.com>
2019-07-11 10:32:01 +0800

    具体产品类

---
+++ src/Design_Pattern/Implements/ConcreteProduct2.java
@@ -0,0 +1,10 @@
+package Design_Pattern.Implements;
+
+import Design_Pattern.Interface.Product;
+
+/**
+ * @Author: xql
+ * @Date: Created in 17:38 2019/7/10
+ */
+public class ConcreteProduct2 implements Product {
+}

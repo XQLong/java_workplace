b2d70708ee7a251b2efc6efeffd259acd2984061
XQLong <xql820148218@gmail.com>
2019-07-11 16:11:59 +0800

    抽象工厂模式抽象产品类

---
+++ src/Design_Pattern/Interface/ProductA.java
@@ -0,0 +1,8 @@
+package Design_Pattern.Interface;
+
+/**
+ * @Author: xql
+ * @Date: Created in 10:42 2019/7/11
+ */
+public interface ProductA {
+}
---
+++ src/Design_Pattern/Interface/ProductB.java
@@ -0,0 +1,9 @@
+package Design_Pattern.Interface;
+
+/**
+ * @Author: xql
+ * @Date: Created in 10:42 2019/7/11
+ * 抽象工厂模式抽象产品类
+ */
+public interface ProductB {
+}

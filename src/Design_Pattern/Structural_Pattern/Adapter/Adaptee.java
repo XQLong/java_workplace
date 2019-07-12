1cfb1c3e664f9c41f80e8c3330518874893b147d
XQLong <xql820148218@gmail.com>
2019-07-12 21:23:49 +0800

    适配器模式中的源类

---
+++ src/Design_Pattern/Structural_Pattern/Adapter/Adaptee.java
@@ -0,0 +1,10 @@
+package Design_Pattern.Structural_Pattern.Adapter;
+
+/**
+ * @Author: xql
+ * @Date: Created in 10:47 2019/7/12
+ * 适配器模式中的源类
+ */
+public class Adaptee {
+    public void sampleOperation1(){}
+}

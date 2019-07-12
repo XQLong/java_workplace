9a0d491564e06a11bb82d6fbf661a523ee0dab66
XQLong <xql820148218@gmail.com>
2019-07-12 21:24:10 +0800

    适配器模式中的适配器类（类适配器模式）

---
+++ src/Design_Pattern/Structural_Pattern/Adapter/Adapter.java
@@ -0,0 +1,13 @@
+package Design_Pattern.Structural_Pattern.Adapter;
+
+/**
+ * @Author: xql
+ * @Date: Created in 10:48 2019/7/12
+ * 适配器模式中的适配器类（类适配器模式）
+ */
+public class Adapter extends Adaptee implements Target{
+    @Override
+    public void sampleOperation2() {
+
+    }
+}

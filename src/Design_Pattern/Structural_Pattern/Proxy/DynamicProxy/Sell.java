c4f5fb9926ed7e6ee58bf7d2d72af19245193c7d
XQLong <xql820148218@gmail.com>
2019-07-19 16:39:55 +0800

    动态代理公共接口类

---
+++ src/Design_Pattern/Structural_Pattern/Proxy/DynamicProxy/Sell.java
@@ -0,0 +1,11 @@
+package Design_Pattern.Structural_Pattern.Proxy.DynamicProxy;
+
+/**
+ * @Author: xql
+ * @Date: Created in 16:09 2019/7/19
+ * 动态代理抽象主题类（公共接口）
+ */
+public interface Sell {
+    void sell();
+    void ad();
+}

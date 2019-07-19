d130e4068e303b69d774958434f2e9bc9ef3efbc
XQLong <xql820148218@gmail.com>
2019-07-19 16:34:02 +0800

    代理模式抽象主题角色

---
+++ src/Design_Pattern/Structural_Pattern/Proxy/Image.java
@@ -0,0 +1,10 @@
+package Design_Pattern.Structural_Pattern.Proxy;
+
+/**
+ * @Author: xql
+ * @Date: Created in 9:36 2019/7/19
+ * 代理模式中的抽象主题角色类
+ */
+public interface Image {
+    void showImage();
+}

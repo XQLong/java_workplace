e47b6ee6b8bab823e3973edc2614ac896d27044d
XQLong <xql820148218@gmail.com>
2019-07-21 20:37:10 +0800

    命令模式调用者类

---
+++ src/Design_Pattern/Behavioral_Pattern/Command/Invoker.java
@@ -0,0 +1,28 @@
+package Design_Pattern.Behavioral_Pattern.Command;
+
+/**
+ * @Author: xql
+ * @Date: Created in 11:36 2019/7/20
+ * 命令模式调用者类
+ */
+public class Invoker {
+    private Command[] onCommands;
+    private Command[] offCommands;
+    private final int slotNum = 7;
+    public Invoker() {
+        this.onCommands = new Command[slotNum];
+        this.offCommands = new Command[slotNum];
+    }
+    public void setOnCommand(Command command, int slot) {
+        onCommands[slot] = command;
+    }
+    public void setOffCommand(Command command, int slot) {
+        offCommands[slot] = command;
+    }
+    public void onButtonWasPushed(int slot) {
+        onCommands[slot].execute();
+    }
+    public void offButtonWasPushed(int slot) {
+        offCommands[slot].execute();
+    }
+}

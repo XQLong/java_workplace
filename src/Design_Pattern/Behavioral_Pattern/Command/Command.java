275a38b6a8be585de7e7ef1fe138d7ae13591d2d
XQLong <xql820148218@gmail.com>
2019-07-21 20:36:45 +0800

    命令模式抽象命令类

---
+++ src/Design_Pattern/Behavioral_Pattern/Command/Command.java
@@ -0,0 +1,10 @@
+package Design_Pattern.Behavioral_Pattern.Command;
+
+/**
+ * @Author: xql
+ * @Date: Created in 11:16 2019/7/20
+ * 命令模式抽象命令类
+ */
+public interface Command {
+    void execute();
+}

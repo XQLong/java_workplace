f12c45d278a018e09beea7d731633a07d77fc1dc
XQLong <xql820148218@gmail.com>
2019-07-21 20:39:41 +0800

    观察者模式抽象观察者类

---
+++ src/Design_Pattern/Behavioral_Pattern/Observer/Observer.java
@@ -0,0 +1,9 @@
+package Design_Pattern.Behavioral_Pattern.Observer;
+
+/**
+ * @Author: xql
+ * @Date: Created in 19:22 2019/7/20
+ */
+public interface Observer {
+    void update(float temp, float humidity, float pressure);
+}

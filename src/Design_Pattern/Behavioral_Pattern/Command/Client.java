506e9ba91f9e87021dc7880c4f23400b2649a1f6
XQLong <xql820148218@gmail.com>
2019-07-21 20:36:20 +0800

    命令模式客户端类

---
+++ src/Design_Pattern/Behavioral_Pattern/Command/Client.java
@@ -0,0 +1,19 @@
+package Design_Pattern.Behavioral_Pattern.Command;
+
+/**
+ * @Author: xql
+ * @Date: Created in 11:43 2019/7/20
+ * 命令模式客户端类
+ */
+public class Client {
+    public static void main(String[] args) {
+        Invoker invoker = new Invoker();
+        Light light = new Light();
+        Command lightOnCommand = new LightOnCommand(light);
+        Command lightOffCommand = new LightOffCommand(light);
+        invoker.setOnCommand(lightOnCommand, 0);
+        invoker.setOffCommand(lightOffCommand, 0);
+        invoker.onButtonWasPushed(0);
+        invoker.offButtonWasPushed(0);
+    }
+}

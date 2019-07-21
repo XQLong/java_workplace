ccfb2282466de06249e9e298e02e8975b07ed17b
XQLong <xql820148218@gmail.com>
2019-07-21 20:41:12 +0800

    客户端

---
+++ src/Design_Pattern/Behavioral_Pattern/Observer/WeatherStation.java
@@ -0,0 +1,17 @@
+package Design_Pattern.Behavioral_Pattern.Observer;
+
+/**
+ * @Author: xql
+ * @Date: Created in 19:28 2019/7/20
+ * 客户端
+ */
+public class WeatherStation {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+        CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+
+        weatherData.setMeasurements(0, 0, 0);
+        weatherData.setMeasurements(1, 1, 1);
+    }
+}

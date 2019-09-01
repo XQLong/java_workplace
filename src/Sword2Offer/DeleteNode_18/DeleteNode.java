cc47e70b2f9e3ef69e37f9fa63fd2df432e371d2
XQLong <xql820148218@gmail.com>
2019-09-01 22:13:56 +0800

    删除链表中重复的节点

--- src/Sword2Offer/DeleteNode_18/DeleteNode.java
+++ src/Sword2Offer/DeleteNode_18/DeleteNode.java
@@ -6,26 +6,6 @@ import Sword2Offer.TestInstance.ListNode;
  * Created by xql on 2019/4/16.
  */
 public class DeleteNode {
-    public ListNode deletenode(ListNode head,ListNode node){
-        if (head==null||head.next==null) return null;
-        ListNode n = head;
-        while (n.next!=null){
-            if (n.next==node){
-                //node节点为末节点
-                if(node.next==null){
-                    n.next = null;
-                    return head;
-                }
-                //node节点不为末节点
-                else {
-                    n.next = node.next;
-                    return head;
-                }
-            }
-            n = n.next;
-        }
-        return head;
-    }
     public ListNode deleterepeatnode(ListNode pHead){
         if (pHead==null||pHead.next==null) return pHead;
         ListNode next = pHead.next;
